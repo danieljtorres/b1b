65fdeaff184e1cee4bbf787f7a718dd17511e2d2
Daniel Torres <danieljtorres94@gmail.com>
2018-06-26 14:45:33 -0300

    eliminar carpetas basura

--- config/sequelize.js
+++ config/sequelize.js
@@ -4,13 +4,15 @@ module.exports = {
         password: ')V6ry[*bzeGA*L,b',
         database: 'b1b',
         host: '144.202.44.43',
-        dialect: 'mysql'
+        dialect: 'mysql',
+        logging: false,
     },
     development: {
         username: process.env.MYSQL_USER || 'root',
         password: process.env.MYSQL_PASSWORD || '',
         database: process.env.MYSQL_DATABASE || 'b1b_dev',
         host: process.env.MYSQL_HOST || '127.0.0.1',
-        dialect: 'mysql'
+        dialect: 'mysql',
+        logging: process.env.MYSQL_LOG || false,
     }
 }[process.env.NODE_ENV]
