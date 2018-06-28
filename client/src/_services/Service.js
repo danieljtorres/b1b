a7c9c33d97c6ac2a99fe98923837d61041c76056
Daniel Torres <danieljtorres94@gmail.com>
2018-06-28 16:02:17 -0300

    return api ya creada

--- client/src/_services/Service.js
+++ client/src/_services/Service.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:90/api/v0/'
+});
+
 export default class Service {
 
     api() {
-        return axios.create({
-            baseURL: 'http://127.0.0.1:90/api/v0/'
-        });
+        return api;
     }
 
     check() {
